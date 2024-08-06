4291a17afe9178316a03a0cc2098c6ab880bb091
Praveen8104 <praveenreddygoli8@gmail.com>
2024-08-06 10:43:29 +0530

    Day-2

---
+++ Day-1/tester/src/components/class1.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export function Demo() {
+    return <h1>Dr. Doom</h1>;
+}
+
+export function Demo1() {
+    return <h1>New Mask, Same Task</h1>;
+}
+
+// export default Demo;
