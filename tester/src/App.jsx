1dbebff0119423faf6c79f8531620d32d0f16de7
Praveen8104 <praveenreddygoli8@gmail.com>
2024-08-06 11:36:45 +0530

    Nothing

--- tester/src/App.jsx
+++ tester/src/App.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react'
 import './App.css'
 import { Demo, Demo1 } from './components/class1'
+import './components/class2.css'
+import styles from './components/sample.module.css'
 
 
 function App() {
@@ -10,7 +12,8 @@ function App() {
       <Demo />
       <Demo1 />
       <div style={{color:'red',fontSize:'40px',fontWeight:'bold'}}>R.D.J</div>
-
+      {/* <div className={styles.div2}>MCU</div> */}
+      {/* <div class="div1">MARVEL</div> */}
     </>
   )
 }
