04b81084fb1237fe1ffb6acb07bc242243d221ac
Praveen8104 <praveenreddygoli8@gmail.com>
2024-08-08 11:10:14 +0530

    chore: Update fonts and icons in compo.css and compo.jsx

--- task1/src/Components/compo.jsx
+++ task1/src/Components/compo.jsx
@@ -1,17 +1,25 @@
 import React from "react";
-import { ImAmazon, ImAppleinc, ImAndroid } from "react-icons/im";
+import {ImAppleinc, ImAndroid, ImGithub} from "react-icons/im";
+import { FaLinkedin, FaGoogle, FaTwitter } from "react-icons/fa";
 import "./compo.css";
 
 export default function Compo() {
     return (
         <>
             <div>
-                <ImAmazon fontSize="50px" style={{ margin: "10px" }} />
-                <ImAppleinc fontSize="50px" style={{ margin: "10px" }} />
-                <ImAndroid fontSize="50px" style={{ margin: "10px" }} />
+                <FaGoogle fontSize="30px" style={{ margin: "10px" }} />
+                <ImAndroid fontSize="30px" style={{ margin: "10px" }} />
+                <ImAppleinc fontSize="30px" style={{ margin: "10px" }} />
             </div>
-            <div className="text">Hello</div>
-            <div className="text">Bye.. Bye...</div>
+            <div className="text">
+                <div className="text1">Hello</div>
+                <div className="text2">Aditya</div>
+            </div>
+            <div>
+                <ImGithub fontSize="30px" style={{ margin: "10px" }} />
+                <FaLinkedin fontSize="30px" style={{ margin: "10px" }} />
+                <FaTwitter fontSize="30px" style={{ margin: "10px" }} />
+            </div>
         </>
     );
 }
